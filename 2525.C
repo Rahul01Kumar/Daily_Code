881253ebefcc84c5f1f2b2cee2c33b7a689efc51
RAHUL KUMAR <114794076+Rahul01Kumar@users.noreply.github.com>
2023-05-17 21:18:18 +0530

    Add files via upload

---
+++ 2525.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+	int i=0;
+	char input[10];
+	char ch[10];
+	printf("Enter the string in ");
+	scanf("%s",&input);
+	printf("Enter the word to be find ");
+	scanf("%s",&ch);
+	
+}
---
+++ bubblesorting1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main()
+{
+	printf("\n**********Array Operations**********");
+	printf("\n1.Insert a value at last position ");
+	printf("\n2.Insert a value at first position ");
+	printf("\n3.Insert a value at given position ");
+	printf("\n4.delete a value at last position ");
+	printf("\n5.delete a value at first position ");
+	printf("\n6.delete a value at given position ");
+	printf("\n7.search an element ");
+	printf("\n8.print max element ");
+	printf("\n9.print min element ");
+	printf("\n10.sort array in ascending order ");
+	printf("\n11.sort array in descending order ");
+
+
+	
+}
